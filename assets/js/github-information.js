cf2e6bc485f2d802cda0b44eab5d499aef517af6
juliancameron79 <juliancameron79@gmail.com>
2020-01-14 14:41:43 +0100

    Fixed bugs for navbar on all .html pages as well as fixed bugs for github.html

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,7 +15,30 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+function repoInformationHTML(repos) {
+	if (repos.length == 0) {
+		return `<div class="clearfix repo-list">No repos!</div>`;
+	}
+
+	var listItemsHTML = repos.map(function(repo) {
+		return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+	});
+
+	return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
 function fetchGitHubInformation(event) {
+	$("#gh-user-data").html("");
+	$("#gh-repo-data").html("");
 	var username = $("#gh-username").val();
 	if (!username) {
 		$("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -28,10 +51,15 @@ function fetchGitHubInformation(event) {
         </div>`
 	);
 
-	$.when($.getJSON(`https://api.github.com/users/${username}`)).then(
-		function(response) {
-			var userData = response;
+	$.when(
+		$.getJSON(`https://api.github.com/users/${username}`),
+		$.getJSON(`https://api.github.com/users/${username}/repos`)
+	).then(
+		function(firstResponse, secondResponse) {
+			var userData = firstResponse[0];
+			var repoData = secondResponse[0];
 			$("#gh-user-data").html(userInformationHTML(userData));
+			$("#gh-repo-data").html(repoInformationHTML(repoData));
 		},
 		function(errorResponse) {
 			if (errorResponse.status === 404) {
@@ -43,3 +71,4 @@ function fetchGitHubInformation(event) {
 		}
 	);
 }
+$(document).ready(fetchGitHubInformation);
